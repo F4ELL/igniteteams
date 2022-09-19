cf8b905ee256220213fa2b84d5c3a9bef08d509e
F4ELL <ferreiraperes1234@gmail.com>
2022-09-19 15:27:05 -0300

    feat: implementing stack navigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'react-native';
 // recurso do react native que insere um loader na parte superior
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme';
 
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
