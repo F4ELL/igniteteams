e7b5089d85e51cfebabc1677fd11bedbd22854c9
F4ELL <ferreiraperes1234@gmail.com>
2022-09-20 13:51:24 -0300

    feat: loading players by group and team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -14,6 +14,8 @@ import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { AppError } from "@utils/AppError";
 import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
     group: string;
@@ -22,7 +24,7 @@ type RouteParams = {
 export function Players() {
     const [ newPlayerName, setNewPlayerName ] = useState('');
     const [ team, setTeam ] = useState('Time A');
-    const [ players, setPlayers ] = useState(['Rafael', 'Miguel']);
+    const [ players, setPlayers ] = useState<PlayerStorageDTO[]>([]);
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
@@ -39,8 +41,6 @@ export function Players() {
 
         try {
             await playerAddByGroup(newPlayer, group);
-            const players = await playersGetByGroup(group);
-            console.log(players);
 
         } catch (error) {
             if(error instanceof AppError) {
@@ -49,6 +49,17 @@ export function Players() {
                 console.log(error);
                 Alert.alert('Nova pessoa', 'Não foi possível adicionar');
             }
+        }
+    }
+
+    async function fetchPlayersByTeam() {
+        try {
+            const playersByTeam = await playersGetByGroupAndTeam(group, team);
+            setPlayers(playersByTeam);
+
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
         }
     }
 
